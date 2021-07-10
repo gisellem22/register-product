a773680cca9381ca2d50fcf504bde2f41dbdce28
gsdossantos <gsdossantos@sodimac.cl>
2021-07-10 15:32:42 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DeliverComponent } from './pages/deliver/deliver.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: 'register', component: RegisterComponent },
+  { path: 'deliver', component: DeliverComponent },
+  { path: '', redirectTo: '/register', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { DeliverComponent } from './pages/deliver/deliver.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    DeliverComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
