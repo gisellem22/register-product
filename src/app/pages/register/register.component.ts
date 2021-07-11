bca3e255d60d2021e87c9c542aa6e0a39d68515f
gsdossantos <gsdossantos@sodimac.cl>
2021-07-11 15:08:08 -0400

    add: form validation

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
 
@@ -9,17 +14,18 @@ import { map, startWith } from 'rxjs/operators';
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-  constructor() {}
-
-  myControl = new FormControl();
+  form: FormGroup;
   options: string[] = ['One', 'Two', 'Three'];
   filteredOptions: Observable<string[]>;
 
+  constructor(public formBuilder: FormBuilder) {}
+
   ngOnInit() {
-    this.filteredOptions = this.myControl.valueChanges.pipe(
-      startWith(''),
-      map((value) => this._filter(value))
-    );
+    // this.filteredOptions = this.myControl.valueChanges.pipe(
+    //   startWith(''),
+    //   map((value) => this._filter(value))
+    // );
+    this.buildForm();
   }
 
   private _filter(value: string): string[] {
@@ -28,5 +34,29 @@ export class RegisterComponent implements OnInit {
     return this.options.filter((option) =>
       option.toLowerCase().includes(filterValue)
     );
+  }
+
+  buildForm() {
+    /* eslint-disable */
+    this.form = this.formBuilder.group({
+      product: ['', [Validators.required]],
+      date: ['', [Validators.required]],
+      origin: ['', [Validators.required]],
+      destiny: ['', [Validators.required]],
+    });
+    /* eslint-enable */
+  }
+
+  onSubmit() {
+    console.log('submit', this.form.value);
+    if (this.form.valid) {
+      this.saveProduct();
+    } else {
+      console.log('open modal');
+    }
+  }
+
+  saveProduct() {
+    console.log('save here');
   }
 }
